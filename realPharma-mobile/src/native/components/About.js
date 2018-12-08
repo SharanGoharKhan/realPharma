57f676161e2a991ab1731ac64f3dcf786b931bfa
Sharan Khan <sharan.gohar@gmail.com>
2018-12-08 19:18:32 +0500

    added button in check meds

--- realPharma-mobile/src/native/components/About.js
+++ realPharma-mobile/src/native/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   Container, Content, Text, H1, H2, H3,
 } from 'native-base';
-import { TextInput } from 'react-native';
+import { TextInput, Button } from 'react-native';
 import Spacer from './Spacer';
 
 const About = () => (
@@ -14,9 +14,15 @@ const About = () => (
       </H1>
       <Spacer size={10} />
       <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-        />
+        style={{ height: 40 }}
+        placeholder="Type here to translate!"
+      />
+      <Spacer size={10} />
+      <Button
+        
+        title="Submit"
+        color="#841584"
+      />
     </Content>
   </Container>
 );
