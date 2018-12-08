7812a86ddea7bdc62f1159108163a96b64568811
Sharan Khan <sharan.gohar@gmail.com>
2018-12-08 19:06:25 +0500

    Changes in Meds Page

--- realPharma-mobile/src/native/components/About.js
+++ realPharma-mobile/src/native/components/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Container, Content, Text, H1, H2, H3,
 } from 'native-base';
+import { TextInput } from 'react-native';
 import Spacer from './Spacer';
 
 const About = () => (
@@ -9,39 +10,13 @@ const About = () => (
     <Content padder>
       <Spacer size={30} />
       <H1>
-        Heading 1
+        Check Medicine
       </H1>
       <Spacer size={10} />
-      <Text>
-        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
-        tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem
-        malesuada magna mollis euismod. Donec sed odio dui.
-        {' '}
-      </Text>
-
-      <Spacer size={30} />
-      <H2>
-        Heading 2
-      </H2>
-      <Spacer size={10} />
-      <Text>
-        Elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-        mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-        magna mollis euismod. Donec sed odio dui.
-        {' '}
-      </Text>
-
-      <Spacer size={30} />
-      <H3>
-        Heading 3
-      </H3>
-      <Spacer size={10} />
-      <Text>
-        Elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-        mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-        magna mollis euismod. Donec sed odio dui.
-        {' '}
-      </Text>
+      <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+        />
     </Content>
   </Container>
 );
